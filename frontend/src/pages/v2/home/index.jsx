29a566ea5c6b523128bf5e5947958b61f1f8bd85
saya <saya@saya.ac.cn>
2020-09-06 22:49:34 +0800

    增加引导页

---
+++ frontend/src/pages/v2/home/index.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './index.less'
+/*
+ * 文件名：index.jsx
+ * 作者：saya
+ * 创建日期：2020/9/6 - 10:46 下午
+ * 描述：
+ */
+
+// 定义组件（ES6）
+class Home extends Component {
+
+  constructor(props){
+    super(props)
+  }
+
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+// 对外暴露
+export default Home;
