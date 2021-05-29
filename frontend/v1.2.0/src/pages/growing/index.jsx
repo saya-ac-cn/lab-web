e46f2ffedf63f5fd7e174d92698a2ba544f32a87
liunengkai <saya@saya.ac.cn>
2021-05-29 23:01:08 +0800

    调整成长历程的顺序

--- frontend/v1.2.0/src/pages/growing/index.jsx
+++ frontend/v1.2.0/src/pages/growing/index.jsx
@@ -21,94 +21,42 @@ class Growing extends Component {
               建设历程
             </div>
             <div className="timeline">
-
-              <div className="timeline-item">
-                <div className="timeline-icon pink">
-                </div>
-                <div className="timeline-content right">
-                  <h3>2021年5月</h3>
-                  <article>因服务器整体到期，域名重新进行通信管理局备案</article>
-                </div>
-              </div>
-
               <div className="timeline-item">
                 <div className="timeline-icon orange">
                 </div>
                 <div className="timeline-content">
-                  <h3>2020年10月</h3>
-                  <div className='des-div'>
-                    <img alt="2019年3月" src={`${process.env.PUBLIC_URL}/picture/growing/psb6.png`}/>
-                    <div className="des-div-text">
-                      第五个里程碑版本。整体重构，持续改进页面布局、丰富完善功能。
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div className="timeline-item">
-                <div className="timeline-icon green">
-                </div>
-                <div className="timeline-content right">
-                  <h3>2019年10月</h3>
-                  <article>对前端部分进行了重构，前端框架从Vue更换到React</article>
+                  <h3>2016年4月</h3>
+                  <article>发布第一个里程碑版本，个人网站正式上线。最原始，最初的版本采用ASP.NET开发，采用ip地址的形式进行访问。追求极简主义，轻量级开发部署。</article>
                 </div>
               </div>
 
               <div className="timeline-item">
                 <div className="timeline-icon blue">
                 </div>
-                <div className="timeline-content">
-                  <h3>2019年3月</h3>
-                  <div className='des-div'>
-                    <img alt="2019年3月" src={`${process.env.PUBLIC_URL}/picture/growing/psb2.png`}/>
-                    <div className="des-div-text">
-                      第四个里程碑版本，时隔一年，网站再次更新。本次更新，主要对页面和功能进行了大幅度改版。前后端完全分离，前端采用Vue，后端采用Java，对静态资源实现了动静分离，加快访问速度。
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div className="timeline-item">
-                <div className="timeline-icon orange">
-                </div>
                 <div className="timeline-content right">
-                  <h3>2017年10月</h3>
-                  <div className='des-div'>
-                    <img alt="2017年10月" src={`${process.env.PUBLIC_URL}/picture/growing/psb4.png`}/>
-                    <div className="des-div-text">
-                      第三个里程碑版本，进一步对网站的用户交互界面进行改版，统一各终端的访问方式，对各种设备能够做到自适应访问。整体框架切换到Laravel，部署运行更加快速。
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div className="timeline-item">
-                <div className="timeline-icon pink">
-                </div>
-                <div className="timeline-content">
-                  <h3>2017年3月</h3>
-                  <div className='des-div'>
-                    <img alt="2017年3月" src={`${process.env.PUBLIC_URL}/picture/growing/psb1.png`}/>
-                    <div className="des-div-text">
-                      第二个里程碑版本，对网站整体进行了大幅度的改版升级，摒弃了早期不合理的设计，引入了全新的UI设计。另外，网站整体从原来的ASP.NET升级到PHP，使用Yii框架开发，数据库也做了相应的迁移。
-                    </div>
-                  </div>
+                  <h3>2016年5月</h3>
+                  <article>前期准备的域名facetub.cn，通过国家工信部域名备案审核，网站就此开启域名访问。</article>
                 </div>
               </div>
 
               <div className="timeline-item">
                 <div className="timeline-icon green">
                 </div>
-                <div className="timeline-content right">
-                  <h3>2016年12月</h3>
-                  <article>早期的域名facetub.cn停止服务，已申请注销。采用全新的域名saya.ac.cn进行访问。</article>
+                <div className="timeline-content">
+                  <h3>2016年7月</h3>
+                  <div className='des-div'>
+                    <img alt="2016年7月" src={`${process.env.PUBLIC_URL}/picture/growing/psb3.png`}/>
+                    <div className="des-div-text">
+                      对网站进行持续更新，本版本对引导页及主页进行了大幅度的改版。更加的简洁，分类进一步完善。
+                    </div>
+                  </div>
                 </div>
               </div>
 
               <div className="timeline-item">
                 <div className="timeline-icon pink">
                 </div>
-                <div className="timeline-content">
+                <div className="timeline-content right">
                   <h3>2016年8月</h3>
                   <div className='des-div'>
                     <img alt="2016年8月" src={`${process.env.PUBLIC_URL}/picture/growing/psb5.png`}/>
@@ -122,12 +70,35 @@ class Growing extends Component {
               <div className="timeline-item">
                 <div className="timeline-icon green">
                 </div>
+                <div className="timeline-content">
+                  <h3>2016年12月</h3>
+                  <article>早期的域名facetub.cn停止服务，已申请注销。采用全新的域名saya.ac.cn进行访问。</article>
+                </div>
+              </div>
+
+              <div className="timeline-item">
+                <div className="timeline-icon pink">
+                </div>
                 <div className="timeline-content right">
-                  <h3>2016年7月</h3>
+                  <h3>2017年3月</h3>
                   <div className='des-div'>
-                    <img alt="2016年7月" src={`${process.env.PUBLIC_URL}/picture/growing/psb3.png`}/>
+                    <img alt="2017年3月" src={`${process.env.PUBLIC_URL}/picture/growing/psb1.png`}/>
                     <div className="des-div-text">
-                      对网站进行持续更新，本版本对引导页及主页进行了大幅度的改版。更加的简洁，分类进一步完善。
+                      第二个里程碑版本，对网站整体进行了大幅度的改版升级，摒弃了早期不合理的设计，引入了全新的UI设计。另外，网站整体从原来的ASP.NET升级到PHP，使用Yii框架开发，数据库也做了相应的迁移。
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="timeline-item">
+                <div className="timeline-icon orange">
+                </div>
+                <div className="timeline-content">
+                  <h3>2017年10月</h3>
+                  <div className='des-div'>
+                    <img alt="2017年10月" src={`${process.env.PUBLIC_URL}/picture/growing/psb4.png`}/>
+                    <div className="des-div-text">
+                      第三个里程碑版本，进一步对网站的用户交互界面进行改版，统一各终端的访问方式，对各种设备能够做到自适应访问。整体框架切换到Laravel，部署运行更加快速。
                     </div>
                   </div>
                 </div>
@@ -136,9 +107,23 @@ class Growing extends Component {
               <div className="timeline-item">
                 <div className="timeline-icon blue">
                 </div>
+                <div className="timeline-content right">
+                  <h3>2019年3月</h3>
+                  <div className='des-div'>
+                    <img alt="2019年3月" src={`${process.env.PUBLIC_URL}/picture/growing/psb2.png`}/>
+                    <div className="des-div-text">
+                      第四个里程碑版本，时隔一年，网站再次更新。本次更新，主要对页面和功能进行了大幅度改版。前后端完全分离，前端采用Vue，后端采用Java，对静态资源实现了动静分离，加快访问速度。
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="timeline-item">
+                <div className="timeline-icon green">
+                </div>
                 <div className="timeline-content">
-                  <h3>2016年5月</h3>
-                  <article>前期准备的域名facetub.cn，通过国家工信部域名备案审核，网站就此开启域名访问。</article>
+                  <h3>2019年10月</h3>
+                  <article>对前端部分进行了重构，前端框架从Vue更换到React</article>
                 </div>
               </div>
 
@@ -146,8 +131,22 @@ class Growing extends Component {
                 <div className="timeline-icon orange">
                 </div>
                 <div className="timeline-content right">
-                  <h3>2016年4月</h3>
-                  <article>发布第一个里程碑版本，个人网站正式上线。最原始，最初的版本采用ASP.NET开发，采用ip地址的形式进行访问。追求极简主义，轻量级开发部署。</article>
+                  <h3>2020年10月</h3>
+                  <div className='des-div'>
+                    <img alt="2019年3月" src={`${process.env.PUBLIC_URL}/picture/growing/psb6.png`}/>
+                    <div className="des-div-text">
+                      第五个里程碑版本。整体重构，持续改进页面布局、丰富完善功能。
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="timeline-item">
+                <div className="timeline-icon pink">
+                </div>
+                <div className="timeline-content">
+                  <h3>2021年5月</h3>
+                  <article>因服务器整体到期，域名重新进行通信管理局备案</article>
                 </div>
               </div>
             </div>
